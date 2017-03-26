9d9bcf7db3fcc921fd699dcdad16e7c0438c0942
shreyas <shreyaskulur@gmail.com>
2017-03-26 11:41:17 +0530

    updated Bot Driver and remote

---
+++ RF_BOT_Driver/RF_BOT_Driver.ino
@@ -0,0 +1,162 @@
+#include <SPI.h>  
+#include "RF24.h" 
+
+#define lf_speed 5   //left front chip enable pin                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+#define rf_speed 10  //right front chip enable pin
+#define lb_speed 6   //left back chip enable pin
+#define rb_speed 11  //right back chip enable pin
+#define lmf 5   //left motor forward
+#define lmb 6   //left motor back
+#define rmf 9   //right motor forward
+#define rmb 10   //left motor back
+
+int l_speed=255; // set left drive speed in the range 0 to 255
+int r_speed=255; // set right drive speed in the range 0 to 255
+
+//turn left
+void left ()
+{
+  digitalWrite(lmf, LOW);
+  digitalWrite(lmb, HIGH);
+  digitalWrite(rmf, HIGH);
+  digitalWrite(rmb, LOW);
+}
+
+//turn right
+void right ()
+{
+  digitalWrite(lmf, HIGH);
+  digitalWrite(lmb, LOW);
+  digitalWrite(rmf, LOW);
+  digitalWrite(rmb, HIGH);
+}
+
+//move forward
+void fwd ()
+{
+  digitalWrite(lmf, HIGH);
+  digitalWrite(lmb, LOW);
+  digitalWrite(rmf, HIGH);
+  digitalWrite(rmb, LOW);
+}
+
+//move backwards
+void rev ()
+{
+  digitalWrite(lmf, LOW);
+  digitalWrite(lmb, HIGH);
+  digitalWrite(rmf, LOW);
+  digitalWrite(rmb, HIGH);
+}
+
+//stop motion
+void stp ()
+{
+  digitalWrite(lmf, LOW);
+  digitalWrite(lmb, LOW);
+  digitalWrite(rmf, LOW);
+  digitalWrite(rmb, LOW);
+}
+
+void motor_init()
+{
+  pinMode(lmf , OUTPUT);
+  pinMode(lmb , OUTPUT);
+  pinMode(rmf , OUTPUT);
+  pinMode(rmb , OUTPUT);
+  analogWrite( lmf, 0);
+  analogWrite( rmf, 0);
+  analogWrite( lmb, 0);
+  analogWrite( rmb, 0);
+}
+
+RF24 myRadio (7, 8); 
+struct package
+{
+  int lx;
+  int ly;
+  int rx;
+  int ry;
+  int l_key;
+  int r_key;
+};
+
+
+byte addresses[][6] = {"0"}; 
+
+
+
+typedef struct package Package;
+Package data;
+
+void setup() 
+{
+  Serial.begin(115200);
+  delay(1000);
+  motor_init();
+  myRadio.begin(); 
+  myRadio.setChannel(115); 
+  myRadio.setPALevel(RF24_PA_LOW);
+  myRadio.setDataRate( RF24_250KBPS ) ; 
+  myRadio.openReadingPipe(1, addresses[0]);
+  myRadio.startListening();
+}
+
+double t = 0;
+
+void loop()  
+{
+
+  if ( myRadio.available()) 
+  {
+    while (myRadio.available())
+    {
+      myRadio.read( &data, sizeof(data) );
+    }
+    Serial.print("\nReceived Package:");
+    Serial.print(data.lx);
+    Serial.print("  ");
+    Serial.print(data.ly);
+    Serial.print("  ");
+    Serial.print(data.rx);
+    Serial.print("  ");
+    Serial.print(data.ry);
+    Serial.println("  ");
+    if(data.ly > 600)
+    {
+      analogWrite(lmb, (data.ly - 512)/2);
+      analogWrite(lmf, 0);
+    }
+    else if(data.ly < 400)
+    {
+      analogWrite(lmf, ((512 - data.ly)/2)-1);
+      analogWrite(lmb, 0);
+    }
+    else
+    {
+      analogWrite(lmb, 0);
+      analogWrite(lmf, 0);
+    }
+
+     if(data.ry > 600)
+    {
+      analogWrite(rmb, (data.ry - 512)/2);
+      analogWrite(rmf, 0);
+    }
+    else if(data.ry < 400)
+    {
+      analogWrite(rmf, ((512 - data.ry)/2)-1);
+      analogWrite(rmb, 0);
+    }
+    else
+    {
+      analogWrite(rmb, 0);
+      analogWrite(rmf, 0);
+    }
+    t=millis();
+  }
+  if(millis() > t+1000)
+    stp();
+  //Serial.print("* ");
+
+}
